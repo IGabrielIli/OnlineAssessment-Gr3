d9ee4aef24993179aa06fcdbc52c704072eabd66
ILIKIDIS GAVRIIL <gabrielilikidis@gmail.com>
2022-05-12 01:54:41 +0300

    Merge branch 'main' into gavriil

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,8 +3,30 @@ import Test from './Core/Test';
 import Home from './Core/Home';
 import Dashboard from './Core/Dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CalendarHeader } from '../CalendarHeader';
+import { Day } from '../Day';
+import { NewEventModal } from '../NewEventModal';
+import { DeleteEventModal } from '../DeleteEventModal';
+import { useDate } from '../hooks/useDate';
 
 class App extends React.Component {
+
+  const [nav, setNav] = useState();
+  const [clicked, setClicked] = useState();
+  const [events, setEvents] = useState(
+    localStorage.getItem('events') ? 
+      JSON.parse(localStorage.getItem('events')) : 
+      []
+  );
+
+  const eventForDate = date => events.find(e => e.date === date);
+  
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(events));
+  }, [events]);
+
+  const { days, dateDisplay } = useDate(events, nav);
+
   render() {
     return (
       <BrowserRouter>
