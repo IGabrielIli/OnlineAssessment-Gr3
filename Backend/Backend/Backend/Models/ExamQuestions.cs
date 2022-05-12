e14e64bb6fd7d1f9779b84d7ea6ec89ff1e96a68
ILIKIDIS GAVRIIL <gabrielilikidis@gmail.com>
2022-05-12 20:33:45 +0300

    ExamQuestion

---
+++ Backend/Backend/Backend/Models/ExamQuestions.cs
@@ -0,0 +1,10 @@
+namespace Backend
+{
+    public class ExamQuestions
+    {
+        public string? ExamId { get; set; }
+        public string? QuestionId { get; set; }
+        
+
+    }
+}
---
+++ Backend/Backend/Backend/Models/Question.cs
@@ -0,0 +1,12 @@
+namespace Backend
+{
+    public class Question
+    {
+        public string? QuestionId { get; set; }
+        public string? QuestionImageUrl { get; set; }
+        public string? QuestionText { get; set; }
+        public string? QuestionType { get; set; }
+        public string? QuestionDifficulty { get; set; }
+        
+    }
+}
