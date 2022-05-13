533f74cda3a99034c701751f1452d05f979e94ff
OFFTKP <parisoplop@gmail.com>
2022-05-13 18:59:34 +0300

    change text input

--- FrontEnd/src/Core/Dashboard.js
+++ FrontEnd/src/Core/Dashboard.js
@@ -85,74 +85,7 @@ class Dashboard extends React.Component {
       case "profile" :
       return(
         <div>
-            <div class="hello">
-              <h1 style={{marginLeft: "60px"}}>Your profilee </h1>
-            </div>
-            <hr class="solid"/>
-            <div class="container">
-        <div class="row">
-            <div class="col-12">
-              
-              
-              <form class="file-upload">
-                <div class="row mb-5 gx-5">
-                  <div class="col-xxl-8 mb-5 mb-xxl-0">
-                    <div class="bg-secondary-soft px-4 py-5 rounded">
-                      <div class="row g-3">
-                        <h4 class="mb-4 mt-0">Contact detail</h4>
-                        
-                        <div class="col">
-                          <label class="form-label">First Name *   </label>
-                          <input type="text" class="form-control" placeholder="" aria-label="First name" value="Scaralet"/>
-                        </div>
-                        <div class="col">
-                          <label class="form-label">Last Name *   </label>
-                          <input type="text" class="form-control" placeholder="" aria-label="Last name" value="Doe"/>
-                        </div>
-                        <div class="col">
-                          <label class="form-label">Mobile number *   </label>
-                          <input type="text" class="form-control" placeholder="" aria-label="Phone number" value="+91 9852 8855 252"/>
-                        </div>
-                        <div class="col">
-                          <label for="inputEmail4" class="form-label">Email *   </label>
-                          <input type="email" class="form-control" id="inputEmail4" value="example@homerealty.com"/>
-                        </div>
-                      </div> 
-                    </div>
-                  </div>
-                </div> 
-                  <div class="row mb-5 gx-5">
-                   <div class="col-xxl-6">
-                    <div class="bg-secondary-soft px-4 py-5 rounded">
-                      <div class="row g-3">
-                        <h4 class="my-4">Change Password</h4>
-                        
-                        <div class="col">
-                          <label for="exampleInputPassword1" class="form-label">Old password *   </label>
-                          <input type="password" class="form-control" id="exampleInputPassword1"/>
-                        </div>
-                        
-                        <div class="col">
-                          <label for="exampleInputPassword2" class="form-label">New password *   </label>
-                          <input type="password" class="form-control" id="exampleInputPassword2"/>
-                        </div>
-                        
-                        <div class="col">
-                          <label for="exampleInputPassword3" class="form-label">Confirm Password *   </label>
-                          <input type="password" class="form-control" id="exampleInputPassword3"/>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div> 
-                
-                <div class="gap-3 d-md-flex justify-content-md-end text-center">
-                   <button type="button" class="btn btn-primary btn-lg">Update profilee</button>
-                </div>
-              </form> 
-            </div>
-          </div>
-          </div>
+          <input type="text" class="answer"></input>
         </div>
       );
 
