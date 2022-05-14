bafc6ce7f8a938fd2b121259d4d700bd29f1ea8b
Nikoleta Fotopoulou <nikoleta.fotopoulou@gmail.com>
2022-05-14 17:15:17 +0300

    answer controller create

---
+++ BackEnd/Backend/Backend/Controllers/AnswerController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnswerController : ControllerBase
+    {
+        private readonly ILogger<AnswerController> _logger;
+
+        public AnswerController(ILogger<AnswerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Create([FromForm]Answer answer)
+        {
+            if (OracleConnect.conn != null && answer.QuestionId != null)
+            {
+                using OracleCommand command = OracleConnect.conn.CreateCommand();
+                command.CommandText =
+                    "Insert into Answer " +
+                    "(AnswerId, QuestionId, AnswerText, IsCorrect) " +
+                    "values(:AnswerId, :QuestionId, :AnswerText, :IsCorrect) ";
+                var hash = Hash.SHA1(answer.AnswerText);
+                command.Parameters.Add(new OracleParameter("AnswerId", hash));
+                command.Parameters.Add(new OracleParameter("QuestionId", answer.QuestionId));
+                command.Parameters.Add(new OracleParameter("AnswerText", answer.AnswerText));
+                command.Parameters.Add(new OracleParameter("IsCorrect", answer.IsCorrect));
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        [HttpGet("byId")]
+        public Answer Get(int id)
+        {
+            Answer answer = new Answer();
+            var rq = OracleConnect.ReaderQuery("Select * from Answer where AnswerId=" + id.ToString());
+            if (rq != null)
+            {
+                rq.Read();
+                answer.AnswerId = rq["AnswerId"].ToString();
+                answer.QuestionId = rq["QuestionId"].ToString();
+                answer.AnswerText = rq["AnswerText"].ToString();
+                answer.IsCorrect = rq["IsCorrect"].ToString();
+                rq.Dispose();
+            }
+            return answer;
+        }
+
+        [HttpGet("byQuestionId")]
+        public IEnumerable<Answer> GetAll(int id)
+        {
+            var rq = OracleConnect.ReaderQuery("Select * from Answer where QuestionId=" + id.ToString());
+            IEnumerable<Answer> answers = new List<Answer>();
+            if (rq != null)
+            {
+                while (rq.Read())
+                {
+                    Answer answer = new Answer();
+                    answer.AnswerId = rq["AnswerId"].ToString();
+                    answer.QuestionId = rq["QuestionId"].ToString();
+                    answer.AnswerText = rq["AnswerText"].ToString();
+                    answer.IsCorrect = rq["IsCorrect"].ToString();
+                    answers = answers.Append(answer);
+                }
+                rq.Dispose();
+            }
+            return answers.ToArray();
+        }
+    }
+}
